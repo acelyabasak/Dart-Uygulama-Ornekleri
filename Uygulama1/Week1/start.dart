d1d3a64e4107d743c8c4ea0ef654c0fb8aeea6eb
acelyabasak <acelyabasak2015@gmail.com>
2022-06-08 15:38:57 +0300

    Update page editor

--- Uygulama1/Week1/start.dart
+++ Uygulama1/Week1/start.dart
@@ -1,21 +1,19 @@
-void main(){
-
+void main() {
 // Bu kod ekranda mesajımı gösterir.
-print("Hello");
-print("Hello" + " Açelya");
-print(2 + 3);
-print("2 + 3");
-print("2" + "3");
+  print("Hello");
+  print("Hello" + " Açelya");
+  print(2 + 3);
+  print("2 + 3");
+  print("2" + "3");
 
 // Değişken Tanımlama
-String verininAdi = "Verinin Değeri";
-String hello = "Hello5";
-print(hello);
-print(verininAdi);
+  String verininAdi = "Verinin Değeri";
+  String hello = "Hello5";
+  print(hello);
+  print(verininAdi);
 
-int money = 15;
-print(money);
+  int money = 15;
+  print(money);
 
-print(hello + ' ' + '$money');
-
+  print(hello + ' ' + '$money');
 }
---
+++ Uygulama1/Week2/condial.dart
@@ -0,0 +1,15 @@
+void main() {
+  int money = 15;
+  String userName = 'Açelya';
+  bool isCustomerRich = true;
+  print("$money" + userName);
+
+  // if else
+  if (money > 10) {
+    print('Sen zenginsin abii');
+  } else {
+    print('Gerekirse simit yeriz :D');
+  }
+
+  money = money - 10;
+}
